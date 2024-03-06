f0e6bc887b9d3b8757cbc7fda528e863373ebb90
Marit Kristine Moe <marit.moe2@ntebb.no>
2024-03-06 10:55:53 +0100

    Part 1.

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,26 @@
-import { useState } from 'react'
-import CreatePost from './components/CreatePost.jsx';
-import Header from './components/Header.jsx'
-import Posts from './components/Posts.jsx'
-import './App.css'
+import { createContext, useState } from "react";
+import CreatePost from "./components/CreatePost.jsx";
+import Header from "./components/Header.jsx";
+import Posts from "./components/Posts.jsx";
+import "./App.css";
 
-const APP_NAME = 'Next-gen Social Media'
+const APP_NAME = "Next-gen Social Media";
+
+const MyContext = createContext();
 
 function App() {
-    const [posts, setPosts] = useState([
-        { title: 'Hello, world!', content: 'React context is great :)' },
-        { title: 'But...', content: 'It\'s a little confusing at first!' },
-    ])
+  const [posts, setPosts] = useState([
+    { title: "Hello, world!", content: "React context is great :)" },
+    { title: "But...", content: "It's a little confusing at first!" },
+  ]);
 
-    return (
-        <>
-            <Header appName={APP_NAME} />
-            <CreatePost posts={posts} setPosts={setPosts} />
-            <Posts posts={posts} />
-        </>
-    )
+  return (
+    <MyContext.Provider value={{ posts, setPosts, APP_NAME }}>
+      <Header />
+      <CreatePost />
+      <Posts />
+    </MyContext.Provider>
+  );
 }
 
-export { App }
+export { App, MyContext };
--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -1,40 +1,53 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { MyContext } from "../App";
 
 const INITIAL_POST = {
-    title: '',
-    content: '',
-}
+  title: "",
+  content: "",
+};
 
-export default function CreatePost({ posts, setPosts }) {
-    const [post, setPost] = useState(INITIAL_POST)
+export default function CreatePost() {
+  const context = useContext(MyContext);
+  const [post, setPost] = useState(INITIAL_POST);
 
-    const handleChange = (e) => {
-        const { name, value } = e.target
-        setPost({
-          ...post,
-          [name]: value,
-        })
-    }
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setPost({
+      ...post,
+      [name]: value,
+    });
+  };
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        setPosts([...posts, post])
-        setPost(INITIAL_POST)
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    context.setPosts([...context.posts, post]);
+    setPost(INITIAL_POST);
+  };
 
-    return (
-        <form onSubmit={handleSubmit}>
-            <label>
-              Title:
-              <input type="text" name="title" onChange={handleChange} value={post.title}></input>
-            </label>
-            <br/>
-            <label>
-              Content:
-              <textarea name="content" onChange={handleChange} value={post.content} cols={50} rows={5}></textarea>
-            </label>
-            <br/>
-            <input type="submit" value="Post!"></input>
-        </form>
-    )
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Title:
+        <input
+          type="text"
+          name="title"
+          onChange={handleChange}
+          value={post.title}
+        ></input>
+      </label>
+      <br />
+      <label>
+        Content:
+        <textarea
+          name="content"
+          onChange={handleChange}
+          value={post.content}
+          cols={50}
+          rows={5}
+        ></textarea>
+      </label>
+      <br />
+      <input type="submit" value="Post!"></input>
+    </form>
+  );
 }
